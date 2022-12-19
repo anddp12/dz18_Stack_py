031f56f3886b8fb8ffcfe450c6b405902d321f6a
anddp12 <anddp12@gmail.com>
2022-12-19 22:26:36 +0200

    stack method hat.

---
+++ main.py
@@ -0,0 +1,32 @@
+class Stack:
+    def __init__(self) -> None:
+        self.__li = []
+    
+    # помещение строки в стек
+    def push(self, value):
+        pass
+
+    # выталкивание строки из стека
+    def pop(self):
+        pass
+
+    # подсчет количества строк в стеке
+    def counting(self):
+        return len(self.__li)
+
+    # проверка пустой ли стек
+    def isEmplyStack(self):
+        pass
+
+    # проверка полный ли стек
+    def isFullStack(self):
+        pass
+
+    # очистка стека
+    def cleaning(self):
+        pass
+
+    # ■ получение значения без выталкивания верхней строки из стека. 
+    def printStack(self):
+        pass
+
